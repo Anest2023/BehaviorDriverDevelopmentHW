f203485ad83b3a422a1df8904b99510255610663
Anest <efremenko.ira.rzn@gmail.com>
2024-09-26 01:00:05 +0300

    Initial commit

---
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -0,0 +1,26 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+    private final SelenideElement heading = $("[data-test-id=]");
+    private final ElementsCollection cards = $$(".list__item div");
+    private final SelenideElement reloadButton = $ ("[data test-id='action-reload']");
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+    }
+    
+
+
+
+}
+
