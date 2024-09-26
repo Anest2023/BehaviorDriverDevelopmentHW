f13a849b3769ad87adca790fb129b06343061aca
Anest <efremenko.ira.rzn@gmail.com>
2024-09-26 11:22:01 +0300

    Add Test

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -21,28 +21,28 @@ public class DashboardPage {
         heading.shouldBe(visible);
     }
 
-}
 
-public int getCardBalance(String maskedCardNumber) {
-    var text = cards.findBy(Condition.text(maskedCardNumber)).gettext();
-    return extractBalance(text);
-}
+    public int getCardBalance(String maskedCardNumber) {
+        var text = cards.findBy(Condition.text(maskedCardNumber)).gettext();
+        return extractBalance(text);
+    }
 
-public TransferPage selectCardToTransfer(DataHelper.Cardinfo cardinfo) {
-    cards.findBy(Condition.attribute("data-test-id",cardInfo.getTestId())),$("button").click();
-    return new TransferPage();
-}
-
-
-public void reloadDashboardPage(){
-    reloadbatton.click();
-    heading.shouldBe(visible);
-}
+    public TransferPage selectCardToTransfer(DataHelper.Cardinfo cardinfo) {
+        cards.findBy(Condition.attribute("data-test-id", cardInfo.getTestId())), $("button").click();
+        return new TransferPage();
+    }
 
 
-private int extractBalance(String text) {
-    var start = text.indexOf(balanceStart);
-    var finish = text.indexOf(balanceFinish);
-    var value = text.substring(start + balanceStart.length(), finish);
-    return Integer.parseInt(value);
+    public void reloadDashboardPage() {
+        reloadbatton.click();
+        heading.shouldBe(visible);
+    }
+
+
+    private int extractBalance(String text) {
+        var start = text.indexOf(balanceStart);
+        var finish = text.indexOf(balanceFinish);
+        var value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
 }
